b924f6e678cfd8e16a4fead444d0cdd75e729c71
Nilton Gabriel <gabriel@gabrimar.com.br>
2025-12-27 10:52:31 -0300

    feat: adicionado keybind C-j git_files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -19,6 +19,7 @@ telescope.setup({})
 local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>n", builtin.find_files)
+vim.keymap.set("n", "<C-p>", builtin.git_files)
 vim.keymap.set("n", "<leader>g", builtin.live_grep)
 vim.keymap.set("n", "<leader><leader>", builtin.buffers)
 vim.keymap.set("n", "<leader>sf", builtin.lsp_document_symbols)
